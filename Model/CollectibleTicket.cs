116f2b0efde95d7ed59245c5b635fb9e1d84dbd8
Daniel Byrne <dbyrne978@gmail.com>
2022-12-31 19:32:21 -0700

    Add CollectibleTicket model

---
+++ Model/CollectibleTicket.cs
@@ -0,0 +1,20 @@
+﻿namespace CollectibleMovieTicketDB.Model
+{
+    /// <summary>
+    /// Represents a template for a collectible ticket
+    /// </summary>
+    public class CollectibleTicket
+    {
+        public int Id { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public string CollectorNumber { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string Variation { get; set; }
+
+        public float EstimatedEbaySalePrice { get; set; }
+    }
+}
